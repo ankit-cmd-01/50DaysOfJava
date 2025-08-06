ed93f8fab53cea1272cd0e23a46ea6717617732c
Ankit Shinde <ankitshinde@Ankits-MacBook-Air.local>
2025-08-07 00:41:23 +0530

    Day 03: DataTypes

---
+++ Day03_DataTypes/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        // Integer type
+        int age = 22;
+        
+        // Floating point type
+        float pi = 3.14f;
+        
+        // Double type
+        double largePi = 3.1415926535;
+        
+        // Character type
+        char grade = 'A';
+        
+        // Boolean type
+        boolean isJavaFun = true;
+
+        // Output all values
+        System.out.println("Age: " + age);
+        System.out.println("Float PI: " + pi);
+        System.out.println("Double PI: " + largePi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun? " + isJavaFun);
+    }
+}
