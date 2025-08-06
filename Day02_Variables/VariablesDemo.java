ceb78b0f2c405bcc844be611a2ec6f4e4f9b94e1
Ankit Shinde <ankitshinde@Ankits-MacBook-Air.local>
2025-08-07 00:03:19 +0530

    Day 02: Variables and Data Types in Java

---
+++ Day02_Variables/VariablesDemo.java
@@ -0,0 +1,11 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        String name = "Ankit";
+        int age = 22;
+        float height = 5.8f;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + "ft");
+    }
+}
