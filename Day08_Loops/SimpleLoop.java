7728b8a4b6ab8507046ee6eeec3784f202e64ad2
Ankit Shinde <ankitshinde@Ankits-MacBook-Air.local>
2025-08-08 20:51:43 +0530

    Day 08: Loops

---
+++ Day08_Loops/SimpleLoop.java
@@ -0,0 +1,8 @@
+public class SimpleLoop {
+    public static void main(String[] args) {
+        // This 'for' loop prints numbers from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count is: " + i);
+        }
+    }
+}
