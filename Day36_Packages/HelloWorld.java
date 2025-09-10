731b7a479167bf9aa8418a1d6d9c6df3452d7161
Ankit Shinde <ankitstorage2023@gmail.com>
2025-09-10 23:04:38 +0530

    Day 36: Packages

---
+++ Day36_Packages/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * This is a simple "Hello, World!" program in Java.
+ * It's the most basic program you can write.
+ */
+public class HelloWorld {
+
+    /**
+     * The main method is the entry point of any Java application.
+     * The code inside this method is executed when the program is run.
+     */
+    public static void main(String[] args) {
+        // This line prints the text "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
+}
